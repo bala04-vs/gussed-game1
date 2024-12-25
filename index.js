688e84904da9d6e09eeb6fac290092d0261370f4
bala04-vs <venkatposina29@gmail.com>
2024-12-25 15:28:45 +0530

    add

---
+++ index.js
@@ -0,0 +1,34 @@
+// let gameResult = document.getElementById("gameResult")
+// let userInput = document.getElementById("userInput")
+// let randomNumber = Math.ceil(Math.random()*100);
+// function chessGuess(){
+//      let  guessedNumber=parseInt(userInput.value);
+//     if (guessedNumber>randomNumber)
+//     {
+//         gameResult.textContent="Too High Try Again";
+//         gameResult.style.color= "#1e217c";
+//     }
+//     else if (guessedNumber<randomNumber)
+//     {
+//         gameResult.textContent="Too Low Try Again";
+//         gameResult.style.backgroundColor= "#1e217c";
+
+//     }
+//     else if(guessedNumber === randomNumber){
+//         gameResult.textContent ="I love YOU UMA NUVVU ANTE CHALA ISTAM ITTLU NEE SANTHOSH";
+//         gameResult.style.backgroundColor="green";
+
+
+//     }
+//     else {
+//         gameResult.textContent="PLEASE PROVIDE A VAILD INPUT";
+//         gameResult.style.backgroundColor = "#1e217c";
+//     }
+
+// }
+
+
+
+
+
+
